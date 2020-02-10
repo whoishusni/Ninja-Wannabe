d7f978761d1975617b1f3d2d3d9a2c450572f7b8
Husni <mohhusnimubaraq@gmail.com>
2020-02-11 00:07:07 +0700

    update lagi

---
+++ Assets/Scripts/SceneMan.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMan : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            exitGame();
+        }
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    public void retryScene()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void mainMenu()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+
+}
